436e5d231a195e1643583735d8398483e5ecbe50
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2018-05-04 20:11:12 +0200

    Move all subommands into cmd to simplify

--- cmd/oscli/main.go
+++ cmd/oscli/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "github.com/glynternet/oscli/cmd/oscli/cmd"
+import (
+	"strings"
+	"log"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	cmd.Execute()
+	cobra.OnInitialize(initConfig)
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+const (
+	appName = "osc"
+
+	keyListenHost   = "listen-host"
+	usageListenHost = "host address to listen on"
+
+	keyListenPort   = "listen-port"
+	usageListenPort = "port to listen on"
+
+	keyRemoteHost   = "remote-host"
+	usageRemoteHost = "address to send any messages to"
+
+	keyRemotePort   = "remote-port"
+	usageRemotePort = "port of the remote host to send any messages to"
+
+	keyLocal   = "local"
+	usageLocal = "send messages to localhost"
+)
+
+var rootCmd = &cobra.Command{
+	Use: appName,
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP(keyLocal, "l", false, usageLocal)
+	rootCmd.PersistentFlags().String(keyListenHost, "", usageListenHost)
+	rootCmd.PersistentFlags().Uint(keyListenPort, 9000, usageListenPort)
+	rootCmd.PersistentFlags().StringP(keyRemoteHost, "r", "", usageRemoteHost)
+	rootCmd.PersistentFlags().Uint(keyRemotePort, 9000, usageRemotePort)
+	err := viper.BindPFlags(rootCmd.PersistentFlags())
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "binding PFlags"))
+	}
+}
+
+// initConfig sets AutomaticEnv in viper to true.
+func initConfig() {
+	viper.AutomaticEnv() // read in environment variables that match
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 }
