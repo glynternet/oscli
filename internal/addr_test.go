3fa92bc72feb0d499f295f0613888e316142be9a
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2018-05-05 13:58:18 +0200

    Don't support osc address with whitespace in middle
    Prior to this change, CleanAddress would support the adreess '/hi hi'
    even though it is not a valid OSC address.

---
+++ internal/addr_test.go
@@ -0,0 +1,57 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCleanAddress(t *testing.T) {
+	for _, test := range []struct {
+		name    string
+		address string
+		cleaned string
+		err     bool
+	}{
+		{
+			name: "zero-values",
+			err:  true,
+		},
+		{
+			name:    "simple valid",
+			address: "/synth1",
+			cleaned: "/synth1",
+		},
+		{
+			name:    "with whitespace 0",
+			address: "/synth1 woop",
+			err:     true,
+		},
+		{
+			name: "with whitespace 1",
+			address: `/synth1
+woop`,
+			err: true,
+		},
+		{
+			name:    "with no slashes",
+			address: "synth2",
+			cleaned: "/synth2",
+		},
+		{
+			name:    "with multiple slashes",
+			address: "//synth2",
+			cleaned: "//synth2",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			cleaned, err := CleanAddress(test.address)
+			assert.Equal(t, test.cleaned, cleaned)
+			if test.err {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
