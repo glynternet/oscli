0fa3a95f9f6f3e95438866cfcca367941492f4de
Glyn Owen Hanmer <glynhanmer@gmail.com>
2019-11-30 18:34:56 +0000

    ✨ responsible shutdown of ReceivePackets (#6)

--- internal/osc/receieve.go
+++ internal/osc/receieve.go
@@ -1,9 +1,11 @@
 package osc
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/sander/go-osc/osc"
@@ -12,7 +14,7 @@ import (
 // ReceivePackets will receive all packets at the given address.
 // Successfully received packets will be handled by the PacketHandler.
 // Errors whilst receiving will be handled by the given ErrorHandler.
-func ReceivePackets(logger *log.Logger, addr string,
+func ReceivePackets(ctx context.Context, logger *log.Logger, addr string,
 	handlePacket PacketHandler,
 	handleReceiveError ErrorHandler,
 ) error {
@@ -20,24 +22,36 @@ func ReceivePackets(logger *log.Logger, addr string,
 	if err != nil {
 		return errors.Wrap(err, "creating listener")
 	}
-	srv := &osc.Server{}
-	defer func() {
-		if err := conn.Close(); err != nil {
-			logger.Print(errors.Wrap(err, "closing listen connection"))
-		}
-	}()
+	srv := &osc.Server{ReadTimeout: time.Second}
 	fmt.Println("Listening on", addr)
-	for {
-		packet, err := srv.ReceivePacket(conn)
-		if err != nil {
-			handleReceiveError(err)
-			continue
-		}
 
-		if packet != nil {
-			handlePacket(packet)
+	for {
+		select {
+		case <-ctx.Done():
+			err = conn.Close()
+			if err != nil {
+				return errors.Wrapf(err, "closing connection")
+			}
+			logger.Println("Listen connection closed")
+			return nil
+		default:
+			packet, err := srv.ReceivePacket(conn)
+			if err != nil {
+				handleError(handleReceiveError, err)
+			}
+
+			if packet != nil {
+				handlePacket(packet)
+			}
 		}
 	}
+}
+
+func handleError(handler ErrorHandler, err error) {
+	if nErr, ok := err.(net.Error); ok && nErr.Timeout() {
+		return
+	}
+	handler(err)
 }
 
 // PacketHandler handles a packet
