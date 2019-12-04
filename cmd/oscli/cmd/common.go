a142ceb5f5527799beb9cec3cb156a11f1d92e48
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2019-12-04 20:59:10 +0000

    ♻️  centralise client creation logic (#14)

--- cmd/oscli/cmd/common.go
+++ cmd/oscli/cmd/common.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"net"
 
+	osc2 "github.com/glynternet/go-osc/osc"
 	"github.com/glynternet/oscli/internal"
 	"github.com/glynternet/oscli/internal/osc"
 	"github.com/pkg/errors"
@@ -18,6 +19,14 @@ func initRemoteHost(localMode bool, remoteHost string) (string, error) {
 	}
 
 	return host, errors.Wrap(verifyHost(host), "verifying host")
+}
+
+func initRemoteClient(localMode bool, remoteHost string, port int) (*osc2.Client, string, error) {
+	host, err := initRemoteHost(localMode, remoteHost)
+	if err != nil {
+		return nil, "", errors.Wrap(err, "initialising remote host")
+	}
+	return osc2.NewClient(host, port), host, nil
 }
 
 // verifyHost checks that the given string can be resolved through the current
